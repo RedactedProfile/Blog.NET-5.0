9a96e5b8d75b8855b66398ab1c3cf0a51a082b57
Kyle Harrison <redactedprofile@gmail.com>
2020-12-06 17:58:34 -0800

    entity context

---
+++ BlogDotNet50/Models/BlogContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogDotNet50.Models
+{
+    public class BlogContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;database=dotnetblog;user=root;password=toor");
+        }
+    }
+
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Display { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
