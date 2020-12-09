8f8bdee44d57f5c3bfd0c99f0ecf3d7d01ee4f84
Kyle Harrison <redactedprofile@gmail.com>
2020-12-09 00:46:09 -0800

    can now navigate with named routes, custom routing, dynamic data driven pages, and queries supplied to the home page now

--- BlogDotNet50/Controllers/HomeController.cs
+++ BlogDotNet50/Controllers/HomeController.cs
@@ -6,23 +6,49 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using BlogDotNet50.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Dynamic;
 
 namespace BlogDotNet50.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly BlogContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, BlogContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var posts = _context.Posts.Include(p => p.Author).ToList();
+            dynamic viewModel = new ExpandoObject();
+            viewModel.Posts = posts;
+            return View(viewModel);
         }
 
+        [Route("read/{id:int:min(1)}", Name = "ReadArticle")]
+        public IActionResult Read(int id)
+        {
+            var query = _context.Posts
+                .Include(p => p.Author);
+
+            Console.WriteLine(query.ToQueryString());
+
+            var post = query
+                .FirstOrDefault(m => m.ID == id);
+
+            dynamic viewModel = new ExpandoObject();
+            viewModel.Post = post;
+
+            return View(viewModel);
+        }
+
+        [Route("privacy", Name = "PrivacyPolicy")]
         public IActionResult Privacy()
         {
             return View();
---
+++ BlogDotNet50/Migrations/20201209064848_InitialCreate.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlogDotNet50.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Display = table.Column<string>(type: "longtext", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "longtext", nullable: true),
+                    Content = table.Column<string>(type: "longtext", nullable: true),
+                    AuthorID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Posts_Authors_AuthorID",
+                        column: x => x.AuthorID,
+                        principalTable: "Authors",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_AuthorID",
+                table: "Posts",
+                column: "AuthorID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
